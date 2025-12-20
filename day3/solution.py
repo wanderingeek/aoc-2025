acd8a28f4956524efb134edaf71bb2a4257005ab
wanderingeek <kishore_shetty@live.in>
2025-12-20 09:40:33 +0530

    Added solution code for Day 3 Part 2. Works for example but too slow on test input.

--- day3/solution.py
+++ day3/solution.py
@@ -1,7 +1,10 @@
+from functools import reduce
+from itertools import combinations
+
 battery_banks = []
 
-# INPUT_FILE = "./day3/test_input.txt"
-INPUT_FILE = "./actual_inputs/d3_input.txt"
+INPUT_FILE = "./day3/test_input.txt"
+# INPUT_FILE = "./actual_inputs/d3_input.txt"
 
 with open(INPUT_FILE) as file_handle:
     for line in file_handle:
@@ -29,3 +32,49 @@ for battery_bank in battery_banks:
     total_output_joltage += max_joltage
 
 print("Part 1 Total Output Joltage =>", total_output_joltage)
+
+
+# Part 2 is essentially finding the best 12-sequence Combination
+# Like from Permutations and Combinations.
+# In combinations, the order of selection matters
+# which is perfect for us
+
+print("---- PART 2 BEGINS ----")
+
+part2_total_output_joltage = 0
+
+# TODO: Multithreading?
+for battery_bank in battery_banks:
+    # Get all possible 12-digit combinations
+    max_joltage_number = -1
+
+    # Below statement will cause OOM issues
+    # joltage_12_combo_list = list(combinations(battery_bank, 12))
+    # Use it as a generator instead and check them one by one and avoid OOM issues
+    for joltage_12_combo in combinations(battery_bank, 12):
+        # convert each digit into str, concatenate to a string
+        # and then convert to an integer and append to the list
+
+        # Below is not very efficient
+        # joltage_number = int("".join([str(x) for x in joltage_12_combo]))
+        # More efficient way of doing this. Thanks Gemini
+        joltage_number = reduce(
+            lambda accumulator, battery_joltage_digit: 10 * accumulator
+            + battery_joltage_digit,
+            joltage_12_combo,
+        )
+
+        if joltage_number > max_joltage_number:
+            max_joltage_number = joltage_number
+
+    print(
+        "For battery bank",
+        battery_bank,
+        "the max joltage is",
+        max_joltage_number,
+    )
+    part2_total_output_joltage += max_joltage_number
+
+print("Part 2 Total Output Joltage =>", part2_total_output_joltage)
+
+print("---- PART 2 ENDS ----")
